427d1c6f98758c210d98f3460116b8e93bf2eac1
Krausz Arnold <arnoldx17@MacBook-Air.local>
2019-05-30 16:02:52 +0200

    New db structure

--- db.sql
+++ db.sql
@@ -5,12 +5,13 @@ CREATE TABLE `routerboards` (
   `locality` varchar(30) COLLATE utf8_bin NOT NULL,
   `username` varchar(30) COLLATE utf8_bin NOT NULL,
   `password` varchar(30) COLLATE utf8_bin NOT NULL,
-  `routerosversion` varchar(30) COLLATE utf8_bin NOT NULL,
-  `boardname` varchar(30) COLLATE utf8_bin NOT NULL,
-  `architecturename` varchar(30) COLLATE utf8_bin NOT NULL,
-  `currentfirmware` varchar(30) COLLATE utf8_bin NOT NULL,
-  `model` varchar(30) COLLATE utf8_bin NOT NULL,
-  `serialnumber` varchar(30) COLLATE utf8_bin NOT NULL
+  `routerosversion` varchar(30) COLLATE utf8_bin DEFAULT '-',
+  `boardname` varchar(30) COLLATE utf8_bin DEFAULT '-',
+  `architecturename` varchar(30) COLLATE utf8_bin DEFAULT '-',
+  `currentfirmware` varchar(30) COLLATE utf8_bin DEFAULT '-',
+  `model` varchar(30) COLLATE utf8_bin DEFAULT '-',
+  `serialnumber` varchar(30) COLLATE utf8_bin DEFAULT '-',
+  `apiok` tinyint(1) NOT NULL DEFAULT '0'
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 ALTER TABLE `routerboards`
